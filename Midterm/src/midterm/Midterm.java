c43d5bc57c7a5fa08bfbd18bdc7c41f0cce5a64a
Jan Raphael O. Petilla <janpetilla@gmail.com>
2016-09-17 14:03:09 +0800

    Initial Commit

---
+++ Midterm/src/midterm/Midterm.java
@@ -0,0 +1,16 @@
+package midterm;
+
+/**
+ *
+ * @author jopetilla
+ */
+public class Midterm {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+      
+    }
+    
+}
